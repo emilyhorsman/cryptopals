807dbe09b090a97b83597e70cea20256ad3b40e1
Emily Horsman <me@emilyhorsman.com>
2018-03-26 11:11:00 -0400

    Add 1.3 solution

--- 3.c
+++ 3.c
@@ -5,15 +5,29 @@
 #include "set1.h"
 
 
+int scoreEnglishCandidate(uchar *str, unsigned int len) {
+    unsigned int numSpaces = 0;
+    for (int i = 0; i < len; i++) {
+        if (str[i] == ' ') {
+            numSpaces++;
+        }
+    }
+    return numSpaces;
+}
+
+
 #ifdef C3
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return 1;
     }
 
+    uchar *bestResult = NULL;
+    int bestScore = 0;
     uchar *a = (uchar *) argv[1];
     int n = strlen((char *) a);
     uchar *decoded = hexDecode(a, n);
+
     for (int k = 32; k <= 126; k++) {
         uchar c = (uchar) k;
         uchar b[n / 2 + 1];
@@ -22,19 +36,16 @@ int main(int argc, char *argv[]) {
             b[i] = c;
         }
 
-        uchar *result = stringXor(a, b, n / 2);
-        int i;
-        int foundUnprintable = 0;
-        for (i = 0; i < n / 2; i++) {
-            int isPrintableChar = result[i] >= 32 && result[i] <= 126;
-            if (!isPrintableChar) {
-                //foundUnprintable = 1;
-                break;
-            }
-        }
-        if (!foundUnprintable) {
-            printf("%c: %s\n", c, stringXor(a, b, n / 2));
+        uchar *result = stringXor(decoded, b, n / 2);
+        int score = scoreEnglishCandidate(result, n / 2);
+        if (score > bestScore) {
+            bestScore = score;
+            bestResult = result;
         }
     }
+
+    printf("Best result: %s\n", bestResult);
+
+    return 0;
 }
 #endif
